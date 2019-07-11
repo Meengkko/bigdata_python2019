0137fc049e2a71e0b05fd1e9118b6e85d7b36479
Chae Mo <you@ddd.com>
2019-07-11 19:41:31 +0900

    7월 11일

---
+++ 01_jump_to_python/python_note.py
@@ -0,0 +1,17 @@
+import math
+
+def solution(progresses, speeds):
+    remain = list(map(lambda x, y: math.ceil((100 - x)/y), progresses, speeds))
+    answer = []
+    count = 1
+    remain.append(9999)
+    for i in range(len(remain)-1):
+        if remain[i] >= remain[i+1]:
+            count += 1
+        else:
+            answer.append(count)
+            count = 1
+    return answer
+
+
+print(solution([97, 30, 55], [1, 30, 5]))

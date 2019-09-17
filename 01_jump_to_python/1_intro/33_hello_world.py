8f4a9e4e493d49326e050e5dee1572f1e368de2b
Chae Mo <you@ddd.com>
2019-09-17 16:57:24 +0900

    9/17

--- 01_jump_to_python/1_intro/33_hello_world.py
+++
@@ -1,2 +0,0 @@
-print("Hello World!")
-print("한글 테스트")
--- 01_jump_to_python/1_intro/first_pycharm_program.py
+++
@@ -1,5 +0,0 @@
-my_long_variable_name = 777
-my_str = "열심히 공부할까?"
-print("안녕")
-print(my_long_variable_name)
-print(my_str.replace("할까?", "하자!"))
